8384c5d002a66899f5894bf7f963f9f0523b624c
Tagellajore <tagelsft9@gmail.com>
2023-07-13 12:24:17 -0400

    adding property to lightbnb

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -181,10 +181,41 @@ const getAllProperties = function (options, limit = '10') {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
+  const queryString = `INSERT INTO properties (owner_id, title, 
+    description, thumbnail_photo_url, cover_photo_url,
+     cost_per_night, street, city, province, post_code, 
+     country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;`;
+  const queryParams = [
+    Number(property.owner_id),
+    String(property.title),
+    String(property.description),
+    String(property.thumbnail_photo_url),
+    String(property.cover_photo_url),
+    String(property.cost_per_night),
+    String(property.street),
+    String(property.city),
+    String(property.province),
+    String(property.post_code),
+    String(property.country),
+    Number(property.parking_spaces),
+    Number(property.number_of_bathrooms),
+    Number(property.number_of_bedrooms)
+  ];
+
+  return pool
+     .query(queryString, queryParams)
+     .then((response) => {
+      return response.rows[0]
+     })
+     .catch((err) => {
+      console.log(err);
+     })
 };
 
 module.exports = {
